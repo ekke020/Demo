c2d380fb07159ec48d4923eeb5dc5b9f8e663db2
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-06 15:16:33 +0200

    Added JWT field

--- demo/src/main/java/com/backend/demo/services/dto/UserDto.java
+++ demo/src/main/java/com/backend/demo/services/dto/UserDto.java
@@ -1,15 +1,23 @@
 package com.backend.demo.services.dto;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class UserDto {
 
     private Long id;
     private String name;
     private String email;
+    private String token;
+
+    public UserDto(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 }
