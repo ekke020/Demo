0e199e36ce27f0df1644a85b9f75a0c4fb834ad8
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-06 15:13:26 +0200

    Refactored structure

--- demo/src/main/java/com/backend/demo/DemoApplication.java
+++ demo/src/main/java/com/backend/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -22,6 +23,7 @@ import java.util.List;
 
 @SpringBootApplication
 @PropertySource("classpath:base64.properties")
+@ConfigurationPropertiesScan("com.backend.demo.config")
 public class DemoApplication {
 
     @Autowired
@@ -32,48 +34,48 @@ public class DemoApplication {
 	}
 
 
-    @Bean
-    public CommandLineRunner demo(OrderService orderService, ProductService productService, UserService userService){
-        return (args) -> {
-            orderService.deleteAll();
-            productService.deleteAll();
-            userService.deleteAll();
-
-            String[] names = {"Adam Andersson", "Berit Bengtsson", "Carl Carlsson"};
-            String[] emails = {"adam@mail.com", "Berit@mail.com", "Carl@mail.com"};
-			String[] passwords = {"12354", "abcde", "test123"};
-            long[] userIds = new long[3];
-            for (int i = 0; i < 3; i++) {
-				UserCreationDto user = new UserCreationDto();
-                user.setName(names[i]);
-                user.setEmail(emails[i]);
-				user.setPassword(passwords[i]);
-                userIds[i] = userService.save(user).getId();
-            }
-            String[] productNames = {"Mango", "Banana", "Pineapple"};
-			String[] base64 = {
-                    env.getProperty("png.mango"),
-                    env.getProperty("png.banana"),
-                    env.getProperty("png.pineapple")
-            };
-            List<Long> productIds = new ArrayList<>();
-            for (int i = 0; i < productNames.length; i++) {
-                ProductCreationDto product = new ProductCreationDto();
-                product.setName(productNames[i]);
-				product.setPrice(BigDecimal.valueOf(Math.random() * 100).setScale(2, RoundingMode.HALF_UP).doubleValue());
-				product.setBase64(base64[i]);
-                productIds.add(productService.save(product).getId());
-            }
-
-            for (int i = 0; i < 3; i++) {
-                OrderCreationDto order = new OrderCreationDto();
-                order.setProductIds(productIds);
-                order.setUserId(userIds[i]);
-                orderService.save(order);
-            }
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(OrderService orderService, ProductService productService, UserService userService){
+//        return (args) -> {
+//            orderService.deleteAll();
+//            productService.deleteAll();
+//            userService.deleteAll();
+//
+//            String[] names = {"Adam Andersson", "Berit Bengtsson", "Carl Carlsson"};
+//            String[] emails = {"adam@mail.com", "Berit@mail.com", "Carl@mail.com"};
+//			String[] passwords = {"12354", "abcde", "test123"};
+//            long[] userIds = new long[3];
+//            for (int i = 0; i < 3; i++) {
+//				UserCreationDto user = new UserCreationDto();
+//                user.setName(names[i]);
+//                user.setEmail(emails[i]);
+//				user.setPassword(passwords[i]);
+//                userIds[i] = userService.save(user).getId();
+//            }
+//            String[] productNames = {"Mango", "Banana", "Pineapple"};
+//			String[] base64 = {
+//                    env.getProperty("png.mango"),
+//                    env.getProperty("png.banana"),
+//                    env.getProperty("png.pineapple")
+//            };
+//            List<Long> productIds = new ArrayList<>();
+//            for (int i = 0; i < productNames.length; i++) {
+//                ProductCreationDto product = new ProductCreationDto();
+//                product.setName(productNames[i]);
+//				product.setPrice(BigDecimal.valueOf(Math.random() * 100).setScale(2, RoundingMode.HALF_UP).doubleValue());
+//				product.setBase64(base64[i]);
+//                productIds.add(productService.save(product).getId());
+//            }
+//
+//            for (int i = 0; i < 3; i++) {
+//                OrderCreationDto order = new OrderCreationDto();
+//                order.setProductIds(productIds);
+//                order.setUserId(userIds[i]);
+//                orderService.save(order);
+//            }
+//
+//        };
+//    }
 
 
 }
