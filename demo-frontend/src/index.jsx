e2b50fb4c7f40cfe78144a35d38657cad189fc48
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-09 22:52:06 +0200

    Removed the Router

--- demo-frontend/src/index.jsx
+++ demo-frontend/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,3 +12,7 @@ root.render(
     <App />
   </Provider>
 );
+
+/* <Provider store={store}>
+<App />
+</Provider> */
