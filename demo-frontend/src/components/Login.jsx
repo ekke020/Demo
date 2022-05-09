a843474571ed19a82d2802407d574bdf3aa81a9e
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-09 22:51:20 +0200

    Added the login logic

--- demo-frontend/src/components/Login.jsx
+++ demo-frontend/src/components/Login.jsx
@@ -1,49 +1,78 @@
-import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { loginUser } from '../features/user/userSlice';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoggedIn } = useSelector((store) => store.user);
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [navigate, isLoggedIn]);
 
-  const login = async (event) => {
+  const login = (event) => {
     event.preventDefault();
-    const response = await axios.post('http://localhost:8080/user/login', {
-      email: email,
-      password: password,
-    });
-    console.log(response);
+    dispatch(loginUser({ email: email, password: password }));
+    setPassword('');
   };
 
+  //TODO: Add validation with react-form and extract input to a separate component
   return (
-    <div className='shadow-xl w-96 mt-4 rounded-xl bg-warm-orange/75'>
-      <form className='flex flex-col items-center' onSubmit={login}>
-        <input
-          className='
-            bg-warm-white text-center mt-6 border-2 rounded-md
+    <div>
+      <div className='shadow-xl w-96 my-4 rounded-xl bg-second-background/75'>
+        <form className='flex flex-col items-center mx-16' onSubmit={login}>
+          <input
+            className='
+            bg-first-background text-center mt-6 border-2 rounded-md
             focus:invalid:border-pink-500 focus:invalid:ring-pink-500
             invalid:border-pink-500 invalid:text-pink-600
-            border-warm-pink w-64 focus:outline-none focus:border-zinc-500'
-          type='email'
-          name='email'
-          autoComplete='off'
-          placeholder='Email'
-          onChange={({ target }) => setEmail(target.value)}
-        />
-        <input
-          className='
-            bg-warm-white text-center mb-6 mt-3 border-2 rounded-md 
-            border-warm-pink w-64 focus:outline-none focus:border-zinc-500'
-          type='password'
-          name='password'
-          placeholder='Password'
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <input
-          type='submit'
-          value='Login'
-          className='rounded shadow border-warm-pink bg-warm-orange text-neutral-100 w-40 mb-6'
-        />
-      </form>
+            border-first-foreground w-64 focus:outline-none focus:border-zinc-500'
+            type='email'
+            name='email'
+            autoComplete='off'
+            placeholder='Email'
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            className='
+            bg-first-background text-center mt-3 border-2 rounded-md
+            border-first-foreground w-64 focus:outline-none focus:border-zinc-500'
+            type='password'
+            name='password'
+            placeholder='Password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Link
+            className='text-xs ml-auto mb-4 underline text-sky-600'
+            to='recovery'
+          >
+            Forgot password ?
+          </Link>
+          <input
+            type='submit'
+            value='Login'
+            className='
+            rounded shadow bg-second-background
+            text-neutral-100 w-40 mb-6 hover:cursor-pointer'
+          />
+        </form>
+      </div>
+      <div className='text-dark-text ml-4'>
+        Dont have an account?{' '}
+        <Link
+          className='text-xs ml-auto mb-2 underline text-sky-600'
+          to='/account/signup'
+        >
+          Sign up
+        </Link>
+      </div>
     </div>
   );
 };
