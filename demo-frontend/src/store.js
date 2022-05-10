b78743b7a56323e31ceeff7d56ab0af4fa5cf7e3
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-10 23:50:11 +0200

    Added loginErrorReducer

--- demo-frontend/src/store.js
+++ demo-frontend/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
+import loginErrorReducer from './features/error/loginErrorSlice';
+
 const store = configureStore({
   reducer: {
     user: userReducer,
+    loginError: loginErrorReducer,
   },
 });
 
