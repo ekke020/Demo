57b245df386b438f12376fed2f7f6c2b9a01c06c
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-10 23:51:56 +0200

    Added validation to the fields and error handling

--- demo-frontend/src/components/Login.jsx
+++ demo-frontend/src/components/Login.jsx
@@ -1,53 +1,62 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { resetError } from '../features/error/loginErrorSlice';
 import { loginUser } from '../features/user/userSlice';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { register, handleSubmit, watch } = useForm();
+  const watcher = watch();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { isLoggedIn } = useSelector((store) => store.user);
+  const error = useSelector((store) => store.loginError);
+
   useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/');
-    }
-  }, [navigate, isLoggedIn]);
+    console.log(error);
+  }, [error]);
 
-  const login = (event) => {
-    event.preventDefault();
-    dispatch(loginUser({ email: email, password: password }));
-    setPassword('');
+  const login = () => {
+    dispatch(loginUser({ email: watcher.email, password: watcher.password }));
   };
-
-  //TODO: Add validation with react-form and extract input to a separate component
+  //TODO: Add animate-spin and deactivate button on login attempt
   return (
     <div>
-      <div className='shadow-xl w-96 my-4 rounded-xl bg-second-background/75'>
-        <form className='flex flex-col items-center mx-16' onSubmit={login}>
+      <div
+        className={`shadow-xl w-96 my-4 rounded-xl transition ease-in duration-200 
+         ${
+           error.isActive ? 'bg-error-background/50' : 'bg-second-background/75'
+         }`}
+      >
+        <form
+          className='flex flex-col items-center mx-16'
+          onSubmit={handleSubmit(login)}
+        >
           <input
+            {...register('email', {
+              required: "Field can't be empty.",
+              pattern: {
+                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: 'Not a valid email',
+              },
+            })}
             className='
-            bg-first-background text-center mt-6 border-2 rounded-md
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-            invalid:border-pink-500 invalid:text-pink-600
-            border-first-foreground w-64 focus:outline-none focus:border-zinc-500'
+            bg-first-background text-center mt-6 border-2 rounded-md text-neutral-100
+            border-first-border w-64 focus:outline-none focus:border-second-border'
             type='email'
             name='email'
             autoComplete='off'
             placeholder='Email'
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
           />
           <input
+            {...register('password', {
+              required: true,
+            })}
             className='
-            bg-first-background text-center mt-3 border-2 rounded-md
-            border-first-foreground w-64 focus:outline-none focus:border-zinc-500'
+            bg-first-background text-center mt-3 border-2 rounded-md text-neutral-100
+            border-first-border w-64 focus:outline-none focus:border-second-border'
             type='password'
             name='password'
             placeholder='Password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
           />
           <Link
             className='text-xs ml-auto mb-4 underline text-sky-600'
@@ -64,7 +73,7 @@ const Login = () => {
           />
         </form>
       </div>
-      <div className='text-dark-text ml-4'>
+      <div className='text-neutral-100 ml-4'>
         Dont have an account?{' '}
         <Link
           className='text-xs ml-auto mb-2 underline text-sky-600'
