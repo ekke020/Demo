442c069139a651474a3aa68e4b235e15448e73f2
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-10 23:52:14 +0200

    Added styling

--- demo-frontend/src/components/Navbar.jsx
+++ demo-frontend/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fingerPrint, home, login } from '../icons/icons';
+import { FingerPrint, Home, Login } from '../icons/icons';
 
 const Navbar = () => {
   const { isLoggedIn } = useSelector((store) => store.user);
 
   return (
-    <nav className='bg-second-background h-10 flex justify-between'>
-      <div className='ml-5 flex'>
-        <button onClick={() => <Link to='/home' />}>{home()}</button>
-      </div>
-      <div className='mr-5 flex'>
-        <button
-          onClick={() => {
-            <Link to='/account/login' />;
-          }}
-        >
-          {login()}
-        </button>
+    <nav
+      className='
+        flex flex-col justify-end w-full sticky 
+        top-0 h-14 z-40 bg-transparent backdrop-blur-sm 
+        hover:bg-first-background/60 transition-bg duration-300 ease-in-out'
+    >
+      <div className='pb-2 flex justify-between border-solid border-b border-second-border/20'>
+        <div className='ml-5 flex'>
+          <Home />
+        </div>
+        <div className='mr-5 flex'>
+          <Login />
+        </div>
       </div>
     </nav>
   );
