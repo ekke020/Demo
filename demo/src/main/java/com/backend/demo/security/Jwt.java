4a2f785e1a1132d5540687cc1ef04ee8079d76c9
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-15 15:46:08 +0200

    Added new jwt methods

--- demo/src/main/java/com/backend/demo/security/Jwt.java
+++ demo/src/main/java/com/backend/demo/security/Jwt.java
@@ -2,45 +2,59 @@ package com.backend.demo.security;
 
 
 import com.backend.demo.config.Properties;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class Jwt {
 
     //TODO: Move all JWT logic to this class
 
-    private final Properties properties;
     private final Key key;
 
     public Jwt(Properties properties) {
-        this.properties = properties;
         key = new SecretKeySpec(Base64.getDecoder().decode(properties.getJwtKey()),
                 SignatureAlgorithm.HS512.getJcaName());
     }
 
     public String extractEmail(String token) {
-        Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-        //TODO: Fix this method.
-        return null;
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
     }
 
-    public String getJWTToken(String username) {
-        String token = Jwts
-                .builder()
-                .setSubject(username)
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claim = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        return resolver.apply(claim);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return buildToken(claims, userDetails.getUsername());
+    }
+
+    private String buildToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 600000))
-                .signWith(key)
-                .compact();
-        return "Bearer " + token;
+                .signWith(key).compact();
     }
-
 }
