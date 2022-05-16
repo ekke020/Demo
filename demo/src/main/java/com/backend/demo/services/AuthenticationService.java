5367bba17a7761cc22262cd722ba8c37d8bffbac
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-16 22:36:22 +0200

    New UserDetailsService implementation

---
+++ demo/src/main/java/com/backend/demo/services/AuthenticationService.java
@@ -0,0 +1,28 @@
+package com.backend.demo.services;
+
+import com.backend.demo.models.User;
+import com.backend.demo.repositories.UserRepository;
+import com.backend.demo.security.SecurityUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new SecurityUser(user);
+    }
+
+}
