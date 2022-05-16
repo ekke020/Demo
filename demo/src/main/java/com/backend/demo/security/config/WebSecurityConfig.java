bb652f097eeea301d6aca7c830b50ad108dcf98e
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-16 22:34:07 +0200

    Changed from WebSecurityConfigurerAdapter to new Bean focused implementation

--- demo/src/main/java/com/backend/demo/security/config/WebSecurityConfig.java
+++ demo/src/main/java/com/backend/demo/security/config/WebSecurityConfig.java
@@ -1,89 +1,42 @@
-package com.backend.demo.security;
+package com.backend.demo.security.config;
 
-import com.backend.demo.services.UserService;
+import com.backend.demo.security.filters.ExceptionHandlerFilter;
+import com.backend.demo.security.filters.JWTAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private final JWTAuthorizationFilter jwtAuthorizationFilter;
     private final ExceptionHandlerFilter exceptionHandlerFilter;
-    private final LoginAuthentication loginAuthentication;
-    private final UserService userService;
 
     public WebSecurityConfig(
             JWTAuthorizationFilter jwtAuthorizationFilter,
-            ExceptionHandlerFilter exceptionHandlerFilter,
-            LoginAuthentication loginAuthentication, UserService userService) {
+            ExceptionHandlerFilter exceptionHandlerFilter) {
         this.jwtAuthorizationFilter = jwtAuthorizationFilter;
         this.exceptionHandlerFilter = exceptionHandlerFilter;
-        this.loginAuthentication = loginAuthentication;
-        this.userService = userService;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .authenticationProvider(loginAuthentication)
-                .userDetailsService(userService);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests((auth) -> auth
                         .antMatchers("/user/login", "/user/add").permitAll()
                         .anyRequest().authenticated()
+
                 )
                 .addFilterBefore(exceptionHandlerFilter, LogoutFilter.class)
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
-
-
-    @Override
-    @Bean
-    protected AuthenticationManager authenticationManager()
-            throws Exception {
-        return super.authenticationManager();
-    }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors().and()
-//                .csrf().disable()
-//                .authorizeHttpRequests((auth) -> auth
-//                        .antMatchers("/user/login").authenticated()
-//                )
-//                .addFilterBefore(new JWTExceptionFilter(), LogoutFilter.class)
-//                .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers("/user/login", "/user/add");
-//    }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
-//    @Bean
-//    public void authenticationManager(AuthenticationManagerBuilder auth) {
-//        auth.authenticationProvider(loginAuthentication);
-//    }
 }
