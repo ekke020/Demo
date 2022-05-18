1ab013ab777bbb137d0350f89ebbb8e5e3fec47e
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-18 22:02:57 +0200

    Removed password encoder

--- demo/src/main/java/com/backend/demo/security/config/WebSecurityConfig.java
+++ demo/src/main/java/com/backend/demo/security/config/WebSecurityConfig.java
@@ -2,12 +2,11 @@ package com.backend.demo.security.config;
 
 import com.backend.demo.security.filters.ExceptionHandlerFilter;
 import com.backend.demo.security.filters.JWTAuthorizationFilter;
-import com.backend.demo.services.PasswordService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -15,17 +14,11 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
 
     private final JWTAuthorizationFilter jwtAuthorizationFilter;
     private final ExceptionHandlerFilter exceptionHandlerFilter;
-
-    public WebSecurityConfig(
-            JWTAuthorizationFilter jwtAuthorizationFilter,
-            ExceptionHandlerFilter exceptionHandlerFilter) {
-        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
-        this.exceptionHandlerFilter = exceptionHandlerFilter;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,18 +26,12 @@ public class WebSecurityConfig {
                 .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests((auth) -> auth
-                        .antMatchers("/user/login", "/user/add").permitAll()
+                        .antMatchers("/login", "/user/add").permitAll()
                         .anyRequest().authenticated()
-
                 )
                 .addFilterBefore(exceptionHandlerFilter, LogoutFilter.class)
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(PasswordService passwordService) {
-        return new Encoder(passwordService);
     }
 
 }
