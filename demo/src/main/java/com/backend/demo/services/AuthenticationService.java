5a2be2557a5d712d724e890651a6045f76fcefe7
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-18 22:01:35 +0200

    Added login method

--- demo/src/main/java/com/backend/demo/services/AuthenticationService.java
+++ demo/src/main/java/com/backend/demo/services/AuthenticationService.java
@@ -2,20 +2,27 @@ package com.backend.demo.services;
 
 import com.backend.demo.models.User;
 import com.backend.demo.repositories.UserRepository;
+import com.backend.demo.security.Jwt;
 import com.backend.demo.security.SecurityUser;
+import com.backend.demo.services.dto.TokenDto;
+import com.backend.demo.services.dto.LoginDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class AuthenticationService implements UserDetailsService {
 
     private final UserRepository userRepository;
-
-    public AuthenticationService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final PasswordService passwordService;
+    private final Jwt jwt;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -25,4 +32,15 @@ public class AuthenticationService implements UserDetailsService {
         return new SecurityUser(user);
     }
 
+    public TokenDto login(LoginDto dto) throws ResponseStatusException {
+        Optional<User> Optional = userRepository.findByEmail(dto.getEmail());
+        if (Optional.isPresent()) {
+            User user = Optional.get();
+            if (passwordService.matches(dto.getPassword(), user.getPassword())) {
+                SecurityUser securityUser = new SecurityUser(user);
+                return new TokenDto(jwt.generateToken(securityUser));
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password and email does not match");
+    }
 }
