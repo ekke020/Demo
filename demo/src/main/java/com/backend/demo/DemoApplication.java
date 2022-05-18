f940f178ac1d2c203003813e51c2984e6fd40637
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-18 10:52:50 +0200

    Cleaned up the class

--- demo/src/main/java/com/backend/demo/DemoApplication.java
+++ demo/src/main/java/com/backend/demo/DemoApplication.java
@@ -1,40 +1,16 @@
 package com.backend.demo;
 
-import com.backend.demo.security.Jwt;
-import com.backend.demo.services.AuthenticationService;
-import com.backend.demo.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetails;
 
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.backend.demo.config")
 public class DemoApplication {
 
-    @Autowired
-    private Jwt jwt;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-
-    @Bean
-    public CommandLineRunner demo(AuthenticationService authenticationService) {
-		return (args) -> {
-			UserDetails user = authenticationService.loadUserByUsername("ekman-johan020@hotmail.com");
-			System.out.println(user);
-			String jws = jwt.generateToken(user);
-			System.out.println(jws);
-//			System.out.println(jwt.extractEmail(jws));
-//			System.out.println(jwt.extractExpiration(jws));
-//			System.out.println(jwt.isTokenExpired(jws));
-		};
-
-	}
 }
