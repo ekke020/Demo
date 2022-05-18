9b301af386bd25de447c71b4dbd9fb8cce5652f2
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-18 22:03:23 +0200

    Changed return value of getAuthorities

--- demo/src/main/java/com/backend/demo/security/SecurityUser.java
+++ demo/src/main/java/com/backend/demo/security/SecurityUser.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @Data
 public class SecurityUser implements UserDetails {
@@ -15,12 +14,12 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(user::getAuthority);
+        return user.getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return user.getHash();
+        return user.getPassword();
     }
 
     @Override
