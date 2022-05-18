aca89db64a11726a866fc66cdbf8bb52f585b582
Johan Ekman <ekman-johan020@hotmail.com>
2022-05-18 21:58:16 +0200

    Changed from @WebMvcTest to @SpringBootTest

--- demo/src/test/java/com/backend/demo/controllers/UserControllerTest.java
+++ demo/src/test/java/com/backend/demo/controllers/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.backend.demo.controllers;
 
 import com.backend.demo.models.User;
 import com.backend.demo.security.Jwt;
+import com.backend.demo.security.SecurityUser;
 import com.backend.demo.services.UserService;
 import com.backend.demo.services.dto.UserCreationDto;
 import com.backend.demo.services.dto.UserDto;
@@ -10,12 +11,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -27,24 +25,27 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @MockBean
+"Changed from @WebMvcTest to @SpringBootTest"
+    @Autowired
     private Jwt jwt;
 
     @MockBean
     private UserService mockUserService;
-    private String jwtToken;
+    private String jws;
 
     @BeforeEach
     public void init() {
         UserDto u1 = new UserDto(1L, "Adam Andersson", "adam@mail.com");
         UserDto u2 = new UserDto(2L, "Berit Bengtsson", "Berit@mail.com");
         UserDto u3 = new UserDto(3L, "Carl Carlsson", "Carl@mail.com");
+        User user = new User(1L, "test", "test@test.com");
+        jws = jwt.generateToken(new SecurityUser(user));
 
         when(mockUserService.findById(1L)).thenReturn(u1);
         when(mockUserService.getAll()).thenReturn(Set.of(u1, u2, u3));
@@ -61,6 +62,7 @@ class UserControllerTest {
     public void getUserById() throws Exception {
         mvc.perform(MockMvcRequestBuilders
                         .get("/user/{id}", 1L)
+                        .header("Authorization", jws)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L));
@@ -69,7 +71,7 @@ class UserControllerTest {
     @Test
     public void getAllUsers() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/user")
-                        .header("Authorization", jwtToken)
+                        .header("Authorization", jws)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(3));
@@ -85,7 +87,7 @@ class UserControllerTest {
                         .post("/user/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(u1)))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L));
     }
 }
